f1419acc1f104edf7bc0634fccd4f0a0bb37d7cc
rehab06052005-tech <rehab06052005@gmail.com>
2026-01-27 15:59:29 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('laundry-v1').then((cache) => cache.addAll(['index.html', 'manifest.json']))
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
